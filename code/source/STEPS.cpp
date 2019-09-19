5457448cb5f58bdcd469d5c1e5b45b7851b7e5d5
Changgang Li <lichgang@sdu.edu.cn>
2019-09-19 12:08:58 +0800

    v0.6.4. Update STEPS::show_informatin_with_leading_time_stamp() to export information in sequential order when parallel simulation is enabled. This fixes the problem of out-of-order output of dynamic information.

--- code/source/STEPS.cpp
+++ code/source/STEPS.cpp
@@ -184,21 +184,15 @@ void STEPS::show_information_with_leading_time_stamp(string info)
     size_t info_size = splitted_info.size();
     if(info_size!=0)
     {
+        string info="";
         string sys_time = get_system_time_stamp_string();
+        info="["+get_toolkit_name()+"]"+sys_time+" "+splitted_info[0]+"\n";
+        for(size_t i=1; i!=info_size; ++i)
+            info+=("["+get_toolkit_name()+"]"+sys_time+" + "+splitted_info[i]+"\n");
         if(log_file.is_open())
-        {
-            log_file<<"["<<get_toolkit_name()<<"]"<<sys_time<<" "<<splitted_info[0]<<endl;
-
-            for(size_t i=1; i!=info_size; ++i)
-                log_file<<"["<<get_toolkit_name()<<"]"<<sys_time<<" + "<<splitted_info[i]<<endl;
-        }
+            log_file<<info;
         else
-        {
-            cout<<"["<<get_toolkit_name()<<"]"<<sys_time<<" "<<splitted_info[0]<<endl;
-
-            for(size_t i=1; i!=info_size; ++i)
-                cout<<"["<<get_toolkit_name()<<"]"<<sys_time<<" + "<<splitted_info[i]<<endl;
-        }
+            cout<<info;
     }
 }
 
