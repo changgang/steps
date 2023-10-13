f41db9fa42877b18380bc199de6c8776425385a2
Changgang Li <lichgang@sdu.edu.cn>
2023-10-13 18:22:52 +0800

    upload latest version of stepspy to pypi

--- python/stepspy-current/setup.py
+++ python/stepspy-current/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stepspy",
-    version="1.6.1",
+    version="1.6.2",
     author="Changgang Li",
     author_email="lichgang@sdu.edu.cn",
     description="Python module of Simulation Toolkit for Electrical Power Systems",
